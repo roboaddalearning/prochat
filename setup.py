0d49620e9b9e1607dcd23aadf4c4bfa55acfba6f
Sandeep Strategy <sandeep@TSFL.IN>
2025-02-17 14:50:50 +0530

    updated1

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="prochat",
+    version="0.0.0",
+    author="sandeep",
+    author_email="sandeep@tsfulms.in",
+    packages=find_packages()
+)
